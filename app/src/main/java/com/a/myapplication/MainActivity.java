97e9c6372a8fa073ff67fa5e068be59e0225ec9c
nadia-am <nadia.amoee@gmail.com>
2020-07-26 21:38:19 +0430

    read file structre for fist time added

--- app/src/main/java/com/a/myapplication/MainActivity.java
+++ app/src/main/java/com/a/myapplication/MainActivity.java
@@ -1,15 +1,14 @@
 package com.a.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -18,22 +17,25 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import jxl.Sheet;
-import jxl.Workbook;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Word> arrayList = new ArrayList<>();
+    String MY_PREFS_NAME = "dictionary";
+    boolean readSuccessfully = false;
+    ConstraintLayout loading_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        order();
+        loading_view = findViewById(R.id.loading_view);
+        loading_view.setVisibility(View.GONE);
+
+        new AsyncTaskRunner().execute();
 
 
 //        final ListView list = findViewById(R.id.list);
@@ -51,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void order (){
+    public void ReadExcelFile (){
         try {
             InputStream myInput;
             AssetManager  assetManager = getAssets();
@@ -68,8 +70,37 @@ public class MainActivity extends AppCompatActivity {
                     Log.e("FileUtils", "Cell Value: " + myCell.toString()+ " Index :" +myCell.getColumnIndex());
                 }
             }
+            readSuccessfully = true;
         } catch (Exception e) {
             e.printStackTrace();
+            readSuccessfully = false;
+        }
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+            boolean ifExcellReaded = prefs.getBoolean("Readexcel", false);
+            loading_view.setVisibility(View.VISIBLE);
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            ReadExcelFile();
+            return "";
+        }
+        @Override
+        protected void onProgressUpdate(String... text) {
+
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            if (readSuccessfully){
+                SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                editor.putBoolean("Readexcel", true);
+                editor.apply();
+            }
+            loading_view.setVisibility(View.GONE);
         }
     }
 }
