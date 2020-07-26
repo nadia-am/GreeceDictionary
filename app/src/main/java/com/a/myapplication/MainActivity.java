c0c7475c287351e6e763637c7e2ae1b926a10727
nadia-am <nadia.amoee@gmail.com>
2020-07-26 20:50:29 +0430

    read excel file from assets added

--- app/src/main/java/com/a/myapplication/MainActivity.java
+++ app/src/main/java/com/a/myapplication/MainActivity.java
@@ -3,13 +3,28 @@ package com.a.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import jxl.Sheet;
+import jxl.Workbook;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Word> arrayList = new ArrayList<>();
@@ -18,18 +33,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ListView list = findViewById(R.id.list);
-        arrayList = new ArrayList<>();
-        Word word = new Word();
-        word.setPersion("تست");
-        word.setGreece("test");
-        arrayList.add(word);
-        arrayList.add(word);
-        arrayList.add(word);
-        arrayList.add(word);
-        arrayList.add(word);
-        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
-        list.setAdapter(customAdapter);
+        order();
 
+
+//        final ListView list = findViewById(R.id.list);
+//        arrayList = new ArrayList<>();
+//        Word word = new Word();
+//        word.setPersion("تست");
+//        word.setGreece("test");
+//        arrayList.add(word);
+//        arrayList.add(word);
+//        arrayList.add(word);
+//        arrayList.add(word);
+//        arrayList.add(word);
+//        CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
+//        list.setAdapter(customAdapter);
+
+    }
+
+    public void order (){
+        try {
+            InputStream myInput;
+            AssetManager  assetManager = getAssets();
+            myInput = assetManager.open("dic.xls");
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            Iterator<Row> rowIter = mySheet.rowIterator();
+            while (rowIter.hasNext()) {
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                Iterator<Cell> cellIter = myRow.cellIterator();
+                while (cellIter.hasNext()) {
+                    HSSFCell myCell = (HSSFCell) cellIter.next();
+                    Log.e("FileUtils", "Cell Value: " + myCell.toString()+ " Index :" +myCell.getColumnIndex());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
